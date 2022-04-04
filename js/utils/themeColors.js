8af1d629017b25399c2df49eea166bdfa821648c
Arcaino <claudio.dzuwa@gmail.com>
2022-04-04 14:12:24 -0300

    feat(itemlist): scroll no componente de lista de itens

--- js/utils/themeColors.js
+++ js/utils/themeColors.js
@@ -3,7 +3,8 @@ const themeColors = {
     primaryColor: "#FFFFFF",
     secundaryColor: "#0039C6",
     tertiaryColor: "#000000",
-    itemColor: "#00000090"
+    itemColor: "#00000090",
+    scrollColor: "#00000025"
 };
 
 export default themeColors;
--- js/views/components/ItemListComponent.js
+++ js/views/components/ItemListComponent.js
@@ -1,3 +1,4 @@
+import themeColors from '../../utils/themeColors.js';
 import CategoryComponent from '../components/CategoryComponent.js';
 import ItemComponent from './ItemComponent.js';
 
@@ -16,13 +17,72 @@ class ItemListComponent extends HTMLElement{
     #render(){
 
         const shadow = this.attachShadow({ mode : 'open' });
+        shadow.appendChild(this.#style());
         shadow.appendChild(this.#html());
     };
+
+    #style(){
+
+        const style = document.createElement('style');
+        style.textContent = `
+
+            .itemListComponent{
+
+                height: calc(100% - 20rem);
+                margin: 1.5rem 0;
+                overflow-y: scroll;
+                width: calc(100% + 1rem);
+                color: #00000000;
+                transition: color .5s;
+            }
+
+            .itemListComponent::-webkit-scrollbar-thumb{
+
+                border-radius: 10rem;
+                -webkit-box-shadow: inset 0 0 0 10px;
+                box-shadow: inset 0 0 0 10px;
+            }
+
+            .itemListComponent:hover{
+
+                color: ${themeColors.scrollColor};
+            }
+
+            .itemListComponent::-webkit-scrollbar{
+
+                width: 5px;
+            }
+
+            .itemListComponent::-webkit-scrollbar-track-piece:start {
+                
+                margin-top: 2rem;
+            }
+
+            .fade{
+                height: 40px;
+                width: 17rem;
+                transform: translate(0%, 0%);
+                transition: .5s;
+                left: 0;
+                overflow: auto;
+                position: absolute;
+                background-color: ${themeColors.primaryColor};
+                -webkit-mask-image: -webkit-gradient(linear, left 10%, left bottom, from(black), to(rgba(0, 0, 0, 0)));
+                z-index: 9;
+            }
+        `;
+
+        return style;
+    }
 
     #html(){
 
         this.#itemListComponent = document.createElement('div');
         this.#itemListComponent.classList.add('itemListComponent'); 
+        this.#itemListComponent.innerHTML = `
+        
+            <span class="fade"></span>
+        `;
 
         return this.#itemListComponent;
     };
